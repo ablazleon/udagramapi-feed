f7fe08a3312b6b08bee08ae8cdf5a6ca1c6811b5
Adrián <adrian.blazquez.leon@alumnos.upm.es>
2020-07-14 23:25:57 +0200

    Update index.router.ts

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -2,7 +2,6 @@ import {Router, Request, Response} from 'express';
 import {FeedRouter} from './feed/routes/feed.router';
 
 const router: Router = Router();
-
 router.use('/feed', FeedRouter);
 
 router.get('/', async (req: Request, res: Response) => {
